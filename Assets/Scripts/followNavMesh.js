6384040e42bcbf3ec0a28bdbfe9fc598e844379c
Zak Larsen <buy@vortexdimensions.com>
2015-03-12 18:20:32 -0600

    Layout map updated with key points.

--- Assets/Scripts/followNavMesh.js
+++ Assets/Scripts/followNavMesh.js
@@ -6,7 +6,7 @@ public var npcChangesSpeed : boolean = false;
 private var agent : NavMeshAgent;
 private var isHiding : boolean = false;
 private var count = 1;
-private var DistanceText : string;
+//private var DistanceText: string;
 
 function Start () {
 	agent = GetComponent.<NavMeshAgent>();
@@ -18,13 +18,16 @@ function Update () {
 	//determine if player is looking at NPC
 	//set isHiging to true
 	//determine closest hide spot
+	//Move to Hide spot
+	//Leave hide spot when reson for hiding ends
 	
 //Change NPC Speed for specific conditions
+	//Change Speed based on distance to Player
 	//while (count < 60) {
 		//agent.speed = count * 0.1;
 		//agent.SetDestination(target.position);
 		//count++;
 	//}
-	DistanceText.text = Vector3.Distance(target.position, transform.position);
+//	DistanceText.text = Vector3.Distance(target.position, transform.position);
 	agent.SetDestination(target.position);
 }
