75a2e46c3b46b272250988bf5777b49763d0e49b
Zak Larsen <buy@vortexdimensions.com>
2015-03-12 19:48:40 -0600

    Added NPC for first trigger point

--- Assets/Scripts/followNavMesh.js
+++ Assets/Scripts/followNavMesh.js
@@ -6,9 +6,11 @@ public var npcChangesSpeed : boolean = false;
 private var agent : NavMeshAgent;
 private var isHiding : boolean = false;
 private var count = 1;
+//var animation: Animation;
 //private var DistanceText: string;
 
 function Start () {
+//    animation = GetComponent.<Animation>();
 	agent = GetComponent.<NavMeshAgent>();
 }
 
@@ -29,5 +31,7 @@ function Update () {
 		//count++;
 	//}
 //	DistanceText.text = Vector3.Distance(target.position, transform.position);
+//	animation["Walk"].enabled = true;
+//	animation["Walk"].weight = 1.0;
 	agent.SetDestination(target.position);
 }
